713e0b4c353809ea6252a97cb63aa9d98c3dbcb2
vladi <vrodriguezalvarez4@gmail.com>
2019-09-30 08:46:16 -0500

    mi primer archivo de netbeans

---
+++ src/javaapplication6/JavaApplication6.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication6;
+
+/**
+ *
+ * @author ALUMNO - FIIS
+ */
+public class JavaApplication6 {
+
+  class usuario{
+      private String nombre;
+      private String ID;
+      
+  }
+    
+}
